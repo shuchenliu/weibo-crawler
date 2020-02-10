a4a77701a87704e90c90debbc425d39332a980f0
Eve Shi <eve.shi@urbanspring.hk>
2020-02-11 03:54:29 +0800

    add pipfile

--- weiboTest.py
+++ weiboTest.py
@@ -33,13 +33,13 @@ def SuperwordRollToTheEnd():
         Transfer_Clicks(driver)
         time.sleep(3)
         elems = driver.find_elements_by_css_selector('div.m-box')
-        print("当前包含超话最大数量:%d,n当前的值为:%d,当n为5无法解析出新的超话" % (len(elems),n))
+        print("当前包含超话最大数量:%d,n当前的值为:%d,当n为15无法解析出新的超话" % (len(elems),n))
         after = len(elems)
         if after > before:
             n = 0
         if after == before:        
             n = n + 1
-        if n == 5:
+        if n == 15:
             print("当前包含最大超话数为：%d" % after)
             break
         if after > timeToSleep:
@@ -53,7 +53,7 @@ def insert_data(elems,path,name):
         sheets = workbook.sheet_names()  # 获取工作簿中的所有表格
         worksheet = workbook.sheet_by_name(sheets[0])  # 获取工作簿中所有表格中的的第一个表格
         rows_old = worksheet.nrows  # 获取表格中已存在的数据的行数       
-        rid = rows_old
+        rid = rows_old + 1
         #用户名
         weibo_username = elem.find_elements_by_css_selector('h3.m-text-cut')[0].text
         weibo_userlevel = "普通用户"
@@ -71,6 +71,8 @@ def insert_data(elems,path,name):
         except:
             weibo_userlevel = "普通用户"
         #微博内容
+        # maybe should get a list of links first, then click into each weibo one by one
+        # elemFull = elem.find_elements_by_xpath("//*[contains(text(), '全文')]").click()
         weibo_content = elem.find_elements_by_css_selector('div.weibo-text')[0].text
         shares = elem.find_elements_by_css_selector('i.m-font.m-font-forward + h4')[0].text
         comments = elem.find_elements_by_css_selector('i.m-font.m-font-comment + h4')[0].text
@@ -100,13 +102,13 @@ def get_current_weibo_data(book_name_xls,name,maxWeibo):
             Transfer_Clicks(driver)
             time.sleep(3)
             elems = driver.find_elements_by_css_selector('div.card.m-panel.card9')
-            print("当前包含微博最大数量：%d,n当前的值为：%d, n值到5说明已无法解析出新的微博" % (len(elems),n))
+            print("当前包含微博最大数量：%d,n当前的值为：%d, n值到15说明已无法解析出新的微博" % (len(elems),n))
             after = len(elems)
             if after > before:
                 n = 0
             if after == before:        
                 n = n + 1
-            if n == 5:
+            if n == 15:
                 print("当前关键词最大微博数为：%d" % after)
                 insert_data(elems,book_name_xls,name)
                 break
@@ -119,44 +121,6 @@ def get_current_weibo_data(book_name_xls,name,maxWeibo):
                 timeToSleep = timeToSleep + 300
                 insert_data(elems,book_name_xls,name) 
                 time.sleep(30) 
-
-#点击超话按钮，获取超话页面
-#一开始就是超话，不用这一坨
-# def get_superWords():
-#     time.sleep(5)
-#     elem = driver.find_element_by_xpath("//*[@class='scroll-box nav_item']/ul/li/span[text()='话题']")
-#     elem.click()
-#     #获取所有超话
-#     SuperwordRollToTheEnd()
-#     elemsOfSuper = driver.find_elements_by_css_selector('div.card.m-panel.card26')   
-#     return elemsOfSuper
-
-#获取超话链接、名称、讨论量、阅读量
-# def get_superwordsUrl():
-#     elemsOfSuper = driver.find_elements_by_css_selector('div.card.m-panel.card4')
-#     superWords_url = []
-#     for i in range(0,len(elemsOfSuper)):
-#         superwordsInfo = []
-#         print("当前获取第%d个超话链接，共有%d个超话"% (i+1,len(elemsOfSuper)))
-#         time.sleep(1)      
-#         element = driver.find_elements_by_css_selector('div.card.m-panel.card26')[i]
-#         name = driver.find_elements_by_css_selector('div.card.m-panel.card26 h3')[i].text
-#         yuedu_taolun = driver.find_elements_by_css_selector('div.card.m-panel.card26 h4:nth-last-child(1)')[i].text
-#         yuedu = yuedu_taolun.split(" ")[0]
-#         taolun = yuedu_taolun.split(" ")[1]
-#         #获取话题名称，话题讨论数，阅读数   
-#         print(name)
-#         print(taolun)
-#         print(yuedu)
-#         #获取超话链接
-#         driver.execute_script('arguments[0].click()',element)
-#         time.sleep(3)
-#         print(driver.current_url)
-#         #把链接和超话信息一起存放于列表中
-#         superwordsInfo = [driver.current_url,name,taolun,yuedu]
-#         superWords_url.append(superwordsInfo)
-#         driver.back()
-#     return superWords_url
 
 #爬虫运行 
 def spider(username,password,driver,book_name_xls,sheet_name_xls,keyword,maxWeibo):
@@ -194,16 +158,7 @@ def spider(username,password,driver,book_name_xls,sheet_name_xls,keyword,maxWeib
             time.sleep(20)
     time.sleep(10)
     driver.get("https://m.weibo.cn/p/index?containerid=1008084882401a015244a2ab18ee43f7772d6f&extparam=%E8%82%BA%E7%82%8E%E6%82%A3%E8%80%85%E6%B1%82%E5%8A%A9&luicode=10000011&lfid=100103type%3D1%26q%3D%E8%82%BA%E7%82%8E%E6%82%A3%E8%80%85%E6%B1%82%E5%8A%A9") 
-    # #搜索关键词
-    # elem = driver.find_element_by_xpath("//*[@class='m-text-cut']").click();
-    # time.sleep(5)
-    # elem = driver.find_element_by_xpath("//*[@type='search']");
-    # elem.send_keys(keyword)
-    # elem.send_keys(Keys.ENTER)
 
-    # #get related keyword
-    # print(driver.find_element_by_xpath("//*[@text='肺炎患者求助超话']"))
-    # elem = driver.find_element_by_xpath("//*[@text='肺炎患者求助超话']").click()
     name = keyword
 
     get_current_weibo_data(book_name_xls,name, maxWeibo) #爬取综合
@@ -211,8 +166,8 @@ def spider(username,password,driver,book_name_xls,sheet_name_xls,keyword,maxWeib
    
     
 if __name__ == '__main__':
-    username = "juzhidangsi66655@163.com" #你的微博登录名
-    password = "bg841754" #你的密码
+    username = os.environ["USER"] #你的微博登录名
+    password = os.environ["PW"] #你的密码
     driver = webdriver.Chrome('./chromedriver')#你的chromedriver的地址
     book_name_xls = "./weibo.xls" #填写你想存放excel的路径，没有文件会自动创建
     sheet_name_xls = '微博数据' #sheet表名
@@ -220,5 +175,3 @@ if __name__ == '__main__':
     keywords = ["肺炎患者求助",] #输入你想要的关键字，可以是多个关键词的列表的形式
     for keyword in keywords:
         spider(username,password,driver,book_name_xls,sheet_name_xls,keyword,maxWeibo)
-
-# juzhidangsi66655@163.com----bg841754----
