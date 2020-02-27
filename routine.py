661152ff2d099b96f9d705988de0a922d262d862
shuchenliu <shuchenliu2014@gmail.com>
2020-02-27 03:45:05 -0800

    Add running routine script

---
+++ routine.py
@@ -0,0 +1,15 @@
+import time
+import subprocess
+from datetime import datetime
+
+cmd = ['python', 'main.py', '-m', 'super']
+minute = 2
+
+while 1:
+    timestr = datetime.now().strftime('%m-%d-%Y, %H:%M:%S')
+    write_time_cmd = ['echo', timestr]
+
+    subprocess.run(write_time_cmd)
+    subprocess.run(cmd)
+    break
+    time.sleep(1000 * 60 * minute)
